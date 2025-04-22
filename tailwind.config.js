d34aa9cc35cec15824b8bbbd83071ded4a1fd376
iaemadrid <31416642+iaemadrid@users.noreply.github.com>
2025-04-22 18:32:13 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-dark': '#4b6b50',
+        'green-light': '#a9c9a4',
+        'gray-dark': '#999999',
+        'gray-light': '#d8d8d8'
+      }
+    },
   },
   plugins: [],
 };
